c3be33b7af60bbf37d040db02c4a422c0c84bff0
Cinthia Iza <cizac1@est.ups.edu.ec>
2021-11-10 20:02:51 -0500

    ApiRest

--- TesisVeterinaria/src/main/java/ec/ups/edu/rest/ApiRest.java
+++ TesisVeterinaria/src/main/java/ec/ups/edu/rest/ApiRest.java
@@ -2,25 +2,32 @@ package ec.ups.edu.rest;
 
 import java.io.IOException;
 
+
+
 import javax.ejb.EJB;
 import javax.faces.bean.SessionScoped;
 import javax.inject.Named;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.MediaType;
 
 
 import ec.ups.edu.ejb.MedicoVeterinarioFacade;
+import ec.ups.edu.ejb.PropietarioFacade;
 import ec.ups.edu.modelo.MedicoVeterinario;
+import ec.ups.edu.modelo.Propietario;
 
 
 @Path("/prueba")
 public class ApiRest {
 
 	@EJB private MedicoVeterinarioFacade ejbMedicoVeterinarioFacade;
+	@EJB private PropietarioFacade ejbPropietarioFacade;
 	
 	public ApiRest() {
 
@@ -59,5 +66,47 @@ public class ApiRest {
 		return Response.ok("No creado").build();
 	}
 
+	@POST
+	@Path("/registraPropietario")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response createPropietario(Propietario propietario) {
+		System.out.println("Ingreso");
+		System.out.println(propietario.toString());
+		try {
+			//ejbPropietarioFacade.create(propietario);
+			return Response.ok("Creado")
+	    			.header("Access-Control-Allow-Origin", "*")
+					.header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+					.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE").build();
+		} catch (Exception e) {
+			// TODO: handle exception
+			return Response.ok("Error")
+	    			.header("Access-Control-Allow-Origin", "*")
+					.header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+					.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE").build();
+		}
+	}
+	
+	@GET  
+	@Path("/texto")
+	@Produces(MediaType.APPLICATION_JSON)
+	
+	public Response Prueba() {
+		System.out.println("Ingreso");
+		try {
+			//ejbPropietarioFacade.create(propietario);
+			return Response.ok("Creado desde tesis veterinaria")
+	    			.header("Access-Control-Allow-Origin", "*")
+					.header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+					.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE").build();
+		} catch (Exception e) {
+			// TODO: handle exception
+			return Response.ok("Error")
+	    			.header("Access-Control-Allow-Origin", "*")
+					.header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+					.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE").build();
+		}
+	}
 	
 }
